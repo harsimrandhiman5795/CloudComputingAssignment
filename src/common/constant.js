2a0a1a6b09b06acbb71fee98c629c34416225823
Harsimran Singh <harsimran.singh5@wipro.com>
2020-06-25 19:11:34 +0530

    app

---
+++ src/common/constant.js
@@ -0,0 +1,13 @@
+export const users = [
+    {
+        id: 'harry@gmail.com',
+        pw: 'harry123'
+    },
+    {
+        id: 'admin@gmail.com',
+        pw: 'admin123'
+    },{
+        id:'shubham@gmail.com',
+        pw:'shubham123'
+    }
+]
